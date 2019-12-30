b2f07beaa52838247cce92eee3a485b51ed73c17
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2019-12-30 20:13:25 +0530

    Update ImageUtil.java

--- src/main/java/com/strandls/file/util/ImageUtil.java
+++ src/main/java/com/strandls/file/util/ImageUtil.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 public class ImageUtil {
 	public static void toWEBP(HttpServletRequest req, File src, File dest) throws Exception {
 		String realPath = req.getServletContext().getRealPath("/WEB-INF/");
-		String command = realPath + "cwebp " + src.getPath() + " -o " + dest.getPath();
+		String command = "/bin/sh " + realPath + "cwebp " + src.getPath() + " -o " + dest.getPath();
 		Runtime.getRuntime().exec(command).waitFor();
 	}
 }
