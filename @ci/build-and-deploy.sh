c5b715669d5ba919383789b1e971f28950ba3e36
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2019-12-30 20:05:57 +0530

    :green_heart: fixing CI Build

--- @ci/build-and-deploy.sh
+++ @ci/build-and-deploy.sh
@@ -1,6 +1,7 @@
 # Download maven-toolbox
 curl -L http://bit.ly/maven-toolbox | bash
 chmod +x @ci/maven-toolbox
+chmod +x src/main/webapp/WEB-INF/cwebp
 
 # Configure and build project
 ./@ci/maven-toolbox configure-properties src/main/resources/config.properties
