1195968d0164dbfc7ae662829762ad4b590da570
sethu1012 <sethu10121994@gmail.com>
2020-05-09 12:24:02 +0530

    :recycle: Generate folders

--- src/main/java/com/strandls/file/util/AppUtil.java
+++ src/main/java/com/strandls/file/util/AppUtil.java
@@ -38,7 +38,7 @@ public class AppUtil {
 	public static boolean generateFile(String command) {
 		boolean isFileGenerated = false;
 		try {
-			File f = new File(command);
+			File f = new File(command.substring(0, command.lastIndexOf(File.separatorChar)));
 			f.mkdirs();
 			isFileGenerated = executeCommandWithExitValue(command);
 		} catch (Exception ex) {
