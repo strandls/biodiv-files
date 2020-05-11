7cf32cae0e42a286705d4e8bb97bcfecd4a6326d
sethu1012 <sethu10121994@gmail.com>
2020-05-11 21:03:29 +0530

    :recycle: Added logger

--- src/main/java/com/strandls/file/scheduler/QuartzJob.java
+++ src/main/java/com/strandls/file/scheduler/QuartzJob.java
@@ -1,5 +1,7 @@
 package com.strandls.file.scheduler;
 
+import java.util.Date;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -9,7 +11,7 @@ public class QuartzJob implements Job {
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		// Do something
-		
+		System.out.println("\n\n***** Date: " + new Date() + " *****\n\n");
 	}
 
 }
