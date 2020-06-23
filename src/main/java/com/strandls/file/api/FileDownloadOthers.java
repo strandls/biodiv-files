908cbe65bd19f186ee7368d6548d8002be8fc08d
Ashish <ashish@halekota.strandls.com>
2020-06-23 19:42:37 +0530

    :fire: created generic file download endpoint

--- src/main/java/com/strandls/file/api/FileDownloadOthers.java
+++ src/main/java/com/strandls/file/api/FileDownloadOthers.java
@@ -6,6 +6,12 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+
 import javax.inject.Inject;
 
 import com.strandls.file.ApiContants;
@@ -31,6 +37,25 @@ public class FileDownloadOthers {
 			ex.printStackTrace();
 			return Response.status(Status.BAD_REQUEST).entity(ex.getMessage()).build();
 		}
+	}
+	
+	@Path(value = "/requestfile")
+	@GET
+	public Response downloadFileGivenPath(@QueryParam("type")String fileType, 
+			@QueryParam("name")String fileName) {
+		String path = null;
+		if(fileType.equalsIgnoreCase("observation")) {
+			path = "/app/data/biodiv/data-archive/listpagecsv";
+		}
+			try {
+				if(Files.exists(Paths.get(path+File.separator+fileName), new LinkOption[]{ LinkOption.NOFOLLOW_LINKS}))
+					return accessService.genericFileDownload(path+File.separator+fileName);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+
+			}
+		return Response.status(Status.BAD_REQUEST).build();
 	}
 
 }
