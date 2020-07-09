062dfddcd2d3ac4671a74cec0cd383a212ca7979
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-07-09 14:34:07 +0530

    :recycle: test

--- src/main/java/com/strandls/file/service/FileUploadService.java
+++ src/main/java/com/strandls/file/service/FileUploadService.java
@@ -68,7 +68,7 @@ public class FileUploadService {
 		String fileExtension = Files.getFileExtension(fileName);
 
 		String folderName = "";
-		if (nestedFolder != null && !nestedFolder.isEmpty()) {
+		if (nestedFolder != null) {
 			folderName += String.join(String.valueOf(File.separatorChar), nestedFolder.split(",")) + File.separatorChar;
 		} else {
 			throw new IOException("Invalid NestedFolder Name");
