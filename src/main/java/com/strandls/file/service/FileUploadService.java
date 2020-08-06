b3e6a3acaf040d284e9062f0eae8e8ee1cc31d2d
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-08-06 08:57:54 +0530

    :bug: fix

--- src/main/java/com/strandls/file/service/FileUploadService.java
+++ src/main/java/com/strandls/file/service/FileUploadService.java
@@ -70,10 +70,8 @@ public class FileUploadService {
 		String folderName = "";
 		if (nestedFolder != null && !nestedFolder.isEmpty()) {
 			folderName += String.join(String.valueOf(File.separatorChar), nestedFolder.split(",")) + File.separatorChar;
-		} else {
-			throw new IOException("Invalid NestedFolder Name");
 		}
-		folderName = "".equals(hashKey) ? UUID.randomUUID().toString() : hashKey;
+		folderName += "".equals(hashKey) ? UUID.randomUUID().toString() : hashKey;
 		if (resourceFolder) {
 			folderName += File.separatorChar + "resources";
 		}
