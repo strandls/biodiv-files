a376a09bf91a40b5faa93f73e84cfdb5157733a9
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-08-25 08:22:40 +0530

    Admin mail fix (#36)
    * :recycle: admin mail fix
    
    * :recycle: architecture change

--- src/main/java/com/strandls/file/scheduler/QuartzJob.java
+++ src/main/java/com/strandls/file/scheduler/QuartzJob.java
@@ -11,6 +11,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -33,6 +34,7 @@ import com.strandls.file.RabbitMqConnection;
 import com.strandls.file.util.ImageUtil;
 import com.strandls.file.util.PropertyFileUtil;
 import com.strandls.mail_utility.model.EnumModel.FIELDS;
+import com.strandls.mail_utility.model.EnumModel.INFO_FIELDS;
 import com.strandls.mail_utility.model.EnumModel.MAIL_TYPE;
 import com.strandls.mail_utility.model.EnumModel.MY_UPLOADS_DELETE_MAIL;
 import com.strandls.mail_utility.producer.RabbitMQProducer;
@@ -100,17 +102,19 @@ public class QuartzJob implements Job {
 					Map<String, Object> data = new HashMap<>();
 					data.put(FIELDS.TYPE.getAction(), MAIL_TYPE.MY_UPLOADS_DELETE_MAIL.getAction());
 					data.put(FIELDS.TO.getAction(), new String[] { userData[0] });
+					data.put(FIELDS.SUBSCRIPTION.getAction(), new Boolean(userData[2]));
 					Map<String, Object> model = new HashMap<>();
 					model.put(MY_UPLOADS_DELETE_MAIL.USERNAME.getAction(), userData[1]);
 					model.put(MY_UPLOADS_DELETE_MAIL.FROM_DATE.getAction(), getFormattedDate(new Date(), -18));
 					model.put(MY_UPLOADS_DELETE_MAIL.TO_DATE.getAction(), getFormattedDate(new Date(), 2));
 					data.put(FIELDS.DATA.getAction(), JsonUtil.unflattenJSON(model));
-					producer.produceMail(RabbitMqConnection.EXCHANGE, RabbitMqConnection.ROUTING_KEY, null,
-							JsonUtil.mapToJSON(data));
+					
 
-					data.put(FIELDS.TO.getAction(), PropertyFileUtil.fetchProperty("config.properties", "mail_bcc").split(","));
+					Map<String, Object> mailData = new HashMap<String, Object>();
+					mailData.put(INFO_FIELDS.TYPE.getAction(), MAIL_TYPE.MY_UPLOADS_DELETE_MAIL.getAction());
+					mailData.put(INFO_FIELDS.RECIPIENTS.getAction(), Arrays.asList(data));
 					producer.produceMail(RabbitMqConnection.EXCHANGE, RabbitMqConnection.ROUTING_KEY, null,
-							JsonUtil.mapToJSON(data));
+							JsonUtil.mapToJSON(mailData));
 				}
 
 				files.forEach(file -> {
@@ -136,9 +140,9 @@ public class QuartzJob implements Job {
 	}
 
 	public static String getUserInfo(Session session, Long id) {
-		String sql = "select email, username from suser where id = ?";
+		String sql = "select email, username, send_notification from suser where id = ?";
 		Object[] userData = (Object[]) session.createNativeQuery(sql).setParameter(1, id).getSingleResult();
-		return userData != null && userData.length == 2 ? String.join(DELIMITER, userData[0].toString(), userData[1].toString()) : null;
+		return userData != null && userData.length == 3 ? String.join(DELIMITER, userData[0].toString(), userData[1].toString(), userData[2].toString()) : null;
 	}
 
 	public static LocalDate getFileCreationDate(Path f) {
